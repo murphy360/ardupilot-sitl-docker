43dadc95098c09d2af8fa31fd7c4bed7a0f34453
Corey Murphy <c.murphy360@gmail.com>
2025-02-26 15:54:45 -0800

    Add logging for directory names in build_and_deploy_image.sh

--- build_and_deploy_image.sh
+++ build_and_deploy_image.sh
@@ -25,6 +25,7 @@ git pull
 for dir in */ ; do
 
     dir_string=${dir##*/}
+    printf "Directory name ${dir_string}\n\n"
     # make it lowercase
     dir_string=${dir_string,,}
     printf "Directory name ${dir_string}\n\n"
