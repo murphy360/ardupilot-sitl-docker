9e753f4cd819edfa6988e82730ef46bd04b2386e
Corey Murphy <c.murphy360@gmail.com>
2025-02-26 18:16:07 -0800

    Fix directory name processing to ensure correct handling of empty results and apply lowercase conversion

--- build_and_deploy_image.sh
+++ build_and_deploy_image.sh
@@ -28,8 +28,8 @@ for dir in */ ; do
 
     result="${dir%"${dir##*[!/]}"}" # extglob-free multi-trailing-/ trim
     result="${result##*/}"                  # remove everything before the last /
-    result=${result:-/}                     # correct for dirname=/ case
-    result_lowercase="${result,,}"
+    result="${result:-/}"                     # correct for dirname=/ case
+    result="${result,,}"
     printf "${result}\n\n"
     printf "Directory name ${dir_string}\n\n"
 
