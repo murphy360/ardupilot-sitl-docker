fe82591e14c88b34994fa8f869bb692d4dba200a
Corey Murphy <c.murphy360@gmail.com>
2025-02-27 07:19:24 -0800

    ``` No code changes detected. ```

--- build_and_deploy_image.sh
+++ build_and_deploy_image.sh
@@ -44,7 +44,7 @@ for dir in */ ; do
         # Stop and remove the Docker container
         print_section "Stopping and removing the Docker container for $image_name..."
         docker ps -a | grep $image_name
-        container=$(docker ps | grep $image_name | awk '{print $1}')
+        container=docker container ls -a | grep $image_name | awk '{print $1}' 
 
         docker stop $image_name
         
