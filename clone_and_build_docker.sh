00501dc05380a7c06db3c04c8e6920fcbe77fa76
Corey Murphy <c.murphy360@gmail.com>
2025-02-27 10:43:39 -0800

    Remove unnecessary build arguments from Docker image build command in clone_and_build_docker.sh

--- clone_and_build_docker.sh
+++ clone_and_build_docker.sh
@@ -5,7 +5,7 @@ git clone --recurse-submodules https://github.com/ArduPilot/ardupilot.git
 cd ardupilot
 
 # Build the Docker image
-docker build . -t ardupilot --build-arg USER_UID=$(id -u) --build-arg USER_GID=$(id -g)
+docker build . -t ardupilot
 
 # Run the Docker container
 docker run --rm -it -v "$(pwd):/ardupilot" -u "$(id -u):$(id -g)" ardupilot:latest bash
