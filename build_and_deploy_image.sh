e30e60183c2c09fa4ced60d9a5ad24a692d8bbc5
Corey Murphy <c.murphy360@gmail.com>
2025-02-27 07:20:35 -0800

    Add output for container ID in build_and_deploy_image.sh for better visibility

--- build_and_deploy_image.sh
+++ build_and_deploy_image.sh
@@ -45,7 +45,7 @@ for dir in */ ; do
         print_section "Stopping and removing the Docker container for $image_name..."
         docker ps -a | grep $image_name
         container=docker container ls -a | grep $image_name | awk '{print $1}' 
-
+        printf "Container ID: ${container}\n\n"
         docker stop $image_name
         
         if [ -n "$container" ]; then 
