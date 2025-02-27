00cfa1da8ce37baa97e59ccd9233514dbf2687d1
Corey Murphy <c.murphy360@gmail.com>
2025-02-27 07:26:12 -0800

    Fix container assignment in build_and_deploy_image.sh to correctly capture container ID

--- build_and_deploy_image.sh
+++ build_and_deploy_image.sh
@@ -44,7 +44,7 @@ for dir in */ ; do
         # Stop and remove the Docker container
         print_section "Stopping and removing the Docker container for $image_name..."
         docker ps -a | grep $image_name
-        container=docker container ls -a | grep $image_name | awk '{print $1}' 
+        container=$(docker container ls -a | grep $image_name | awk '{print $1}') 
         printf "Container ID: ${container}\n\n"
         docker stop $image_name
         
