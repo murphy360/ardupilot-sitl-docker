6db341bc0bfd65c480120e31552b87ab7112a84a
Corey Murphy <c.murphy360@gmail.com>
2025-02-27 07:30:02 -0800

    Add temporary exit for testing in build_and_deploy_image.sh

--- build_and_deploy_image.sh
+++ build_and_deploy_image.sh
@@ -45,6 +45,8 @@ for dir in */ ; do
         print_section "Stopping and removing the Docker container for $image_name..."
         container=$(docker container ls -a | grep $image_name | awk '{print $1}') 
         printf "Container ID: ${container}\n\n"
+        # Stop script now for testing purposes
+        exit 0
         docker stop $image_name
         
         if [ -n "$container" ]; then 
